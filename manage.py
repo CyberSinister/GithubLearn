38fc75fba9b9ee54f9d4ae07fae7147491728765
CyberSinister <sinistercipher@yandex.com>
2024-11-13 00:42:59 +0500

    something new form shaf

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GithubLearn.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.managementfdsaasdf aads fasdf asd fasasf import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -32,3 +32,7 @@ da(lastName="john"):
     
 # end def
     main()
+    
+    
+    
+    soimetyhing new from Shaf
