7ff69f75d13e03ea91e87f3646f7149ec16a2e74
CyberSinister <sinistercipher@yandex.com>
2024-11-13 00:37:18 +0500

    using sales order api in core app

--- ignored-file.py
+++ ignored-file.py
@@ -1,1 +1,86 @@
 Hello something else
+
+
+<template>
+<div class='filter-box'>
+<div class='left-box'>
+<el-input placeholder='请输入授权名称' v-model='filter.name' class='common-input' @input='query' clearable suffix-icon='el-icon-search' />
+</div>
+<div class='right-box'>
+ <el-button type='primary'>新建授权申请</el-button>
+</div>
+ </div>
+</template>
+    
+<script lang='ts'>
+import { defineComponent, reactive } from 'vue'
+    
+export default defineComponent({
+name: 'filter-box',
+emits: ['query'],
+setup(props, _) {
+const filter = reactive({
+  name: ''
+})
+  const query = () => {
+ _.emit('query', filter)
+}
+  return {
+  filter,
+  query
+ }
+}
+})
+</script>
+    
+<style lang='stylus' scoped>
+.filter-box
+  display flex
+  height 32px
+  justify-content space-between
+  margin-bottom 16px
+</style>
+    sdf
+    : x = {'apple', 'banana', 'cherry'}
+    y = {'google', 'microsoft', 'apple'}
+    x.symmetric_difference_update(y) 
+    print(x)<template>
+    <div class='filter-box'>
+    <div class='left-box'>
+    <el-input placeholder='请输入授权名称' v-model='filter.name' class='common-input' @input='query' clearable suffix-icon='el-icon-search' />
+    </div>
+    <div class='right-box'>
+     <el-button type='primary'>新建授权申请</el-button>
+    </div>
+     </div>
+    </template>
+        
+    <script lang='ts'>
+    import { defineComponent, reactive } from 'vue'
+        
+    export default defineComponent({
+    name: 'filter-box',
+    emits: ['query'],
+    setup(props, _) {
+    const filter = reactive({
+      name: ''
+    })
+      const query = () => {
+     _.emit('query', filter)
+    }
+      return {
+      filter,
+      query
+     }
+    }
+    })
+    </script>
+        
+    <style lang='stylus' scoped>
+    .filter-box
+      display flex
+      height 32px
+      justify-content space-between
+      margin-bottom 16px
+    </style>
+        
